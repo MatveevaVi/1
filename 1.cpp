e2edb46bc66dbec2e3d5b401d1d3c98c7cdc1070
MatveevaVi <matveevaviktoriya27@gmail.com>
2025-09-26 19:03:24 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() [
+    int a,w;
+    char c;
+    cout<<"Введіть висоту";
+    cin>>a;
+    cout<<"\nВведіть ширину";
+    cin>>w;
+    cout<<"\nВведіть символ";
+    cin>>wc
+    for (int i=0; i<a; i++){
+        for (int j=0;j<w;j++){
+            if(i==0|| i==a-1 || j==0 || j==w-1)
+            cout<<c;
+        else
+            cout<<"";
+        }
+    }
+]
